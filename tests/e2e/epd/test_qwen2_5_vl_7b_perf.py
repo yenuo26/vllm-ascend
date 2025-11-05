96708a6afdec96e1b849f1359a4488a9f2a136d3
wangyu31577 <wangyu31577@hundsun.com>
2025-11-05 09:31:10 +0800

    add pd merge case
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/epd/test_qwen2_5_vl_7b_perf.py
+++ tests/e2e/epd/test_qwen2_5_vl_7b_perf.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import copy
 
-from tests.e2e.conftest import RemoteEPDServer
+from tests.e2e.conftest import RemoteOpenAIServer
 from tests.e2e.epd.conftest import load_config
 from tools.aisbench import run_aisbench_cases
 
@@ -14,6 +14,104 @@ DATASET_PATH = load_config().get("dataset_path")
 TENSOR_PARALLELS = [1]
 
 SHARED_STORAGE_PATH = "/dev/shm/epd/storage"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+async def test_pd_merge_001(model: str, tp_size: int):
+    #TODO PD合并脚本待调试
+    vllm_server_args = [
+        "--no-enable-prefix-caching", "--model", model,
+        "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "30000", "--max-num-batched-tokens",
+        "40000", "--max-num-seqs", "1", "--enforce-eager",
+        "--gpu-memory-utilization", "0.95"
+    ]
+
+    warmup_cases = [{
+        "case_type":
+            "performance",
+        "dataset_path":
+            os.path.join(DATASET_PATH, "simulate_truth"),
+        "request_conf":
+            "vllm_api_stream_chat",
+        "dataset_conf":
+            "textvqa/textvqa_gen",
+        "num_prompts":
+            100,
+        "max_out_len":
+            256,
+        "batch_size":
+            16,
+        "temperature":
+            0.5,
+        "top_k":
+            10,
+        "top_p":
+            0.7,
+        "repetition_penalty":
+            1.2,
+        "request_rate":
+            0,
+        "seed":
+            77,
+        }]
+
+    request_rate = [0.28, 0.78, 1.28, 1.78, 2.28, 2.78, 3.28]
+    case_dict = {
+        "case_type":
+        "performance",
+        "dataset_path":
+        os.path.join(DATASET_PATH, "simulate_truth"),
+        "request_conf":
+        "vllm_api_stream_chat",
+        "dataset_conf":
+        "textvqa/textvqa_gen",
+        "num_prompts":
+        200,
+        "max_out_len":
+        256,
+        "batch_size":
+        128,
+        "temperature":
+        0.5,
+        "top_k":
+        10,
+        "top_p":
+        0.7,
+        "repetition_penalty":
+        1.2,
+        "request_rate":
+        0.28,
+        "baseline":
+        1,
+        "seed":
+        77,
+        "result_file_name":
+        "qwen2_5_vl_7b_perf_custom_1E1PD_merge",
+        "threshold":
+        0.97
+        }
+    aisbench_cases = []
+    for i in range(len(request_rate)):
+        case_dict["request_rate"] = request_rate[i]
+        new_case_dict = copy.deepcopy(case_dict)
+        aisbench_cases.append(new_case_dict)
+
+    api_port = 10001
+    async with RemoteOpenAIServer(
+                                model,
+                               vllm_server_args,server_port=api_port,auto_port=False) as server:
+
+        # warm up
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           aisbench_cases=warmup_cases,verify=False,save=False)
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           aisbench_cases=aisbench_cases)
 
 
 # @pytest.mark.asyncio
@@ -117,6 +215,7 @@ SHARED_STORAGE_PATH = "/dev/shm/epd/storage"
 #                                pd_num=1,
 #                                e_num=1,
 #                                is_epd_same_card=True,
+#                                is_custom_max_token=True,
 #                                e_serve_args=e_server_args,
 #                                pd_serve_args=pd_server_args) as server:
 #
@@ -230,6 +329,7 @@ async def test_1e1pd_001(model: str, tp_size: int):
                                api_server_port=api_port,
                                pd_num=1,
                                e_num=1,
+                               is_custom_max_token=True,
                                e_serve_args=e_server_args,
                                pd_serve_args=pd_server_args) as server:
         # warm up
@@ -341,6 +441,7 @@ async def test_1e2pd_001(model: str, tp_size: int):
                                api_server_port=api_port,
                                pd_num=2,
                                e_num=1,
+                               is_custom_max_token=True,
                                e_serve_args=e_server_args,
                                pd_serve_args=pd_server_args) as server:
         # warm up
