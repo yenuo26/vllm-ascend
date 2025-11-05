cd3a5b49a414578be057628021fc3a79eb2fd490
z00939163 <zhumingjue@huawei.com>
2025-11-05 16:17:36 +0800

    新增参数校验用例

--- tests/e2e/epd/test_ttft_tracing.py
+++ tests/e2e/epd/test_ttft_tracing.py
@@ -127,3 +127,116 @@ async def test_base_001(model: str, tp_size: int):
         run_aisbench_cases(model=model,
                            port=api_port,
                            aisbench_cases=aisbench_cases)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+async def test_base_002(model: str, tp_size: int):
+    e_server_args = [
+        "--no-enable-prefix-caching", "--model", model,
+        "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "30000", "--max-num-batched-tokens",
+        "40000", "--max-num-seqs", "1", "--enforce-eager",
+        "--gpu-memory-utilization", "0.0", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_producer"}'
+    ]
+
+    pd_server_args = [
+        "--model", model, "--max-model-len", "30000",
+        "--max-num-batched-tokens", "40000", "--tensor-parallel-size",
+        str(tp_size), "--max-num-seqs", "128", "--gpu-memory-utilization",
+        "0.95", "--enforce-eager", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_consumer"}'
+    ]
+
+    env_dict = {"TIMECOUNT_ENABLED": "1","VLLM_LOG_STATS_INTERVAL": "10"}
+
+    warmup_cases = [{
+        "case_type":
+            "performance",
+        "dataset_path":
+            os.path.join(DATASET_PATH, "simulate_truth"),
+        "request_conf":
+            "vllm_api_stream_chat",
+        "dataset_conf":
+            "textvqa/textvqa_gen",
+        "num_prompts":
+            100,
+        "max_out_len":
+            256,
+        "batch_size":
+            16,
+        "temperature":
+            0.5,
+        "top_k":
+            10,
+        "top_p":
+            0.7,
+        "repetition_penalty":
+            1.2,
+        "request_rate":
+            0,
+        "seed":
+            77,
+    }]
+
+    request_rate = [0.28, 0.78, 1.28, 1.78, 2.28, 2.78, 3.28]
+    case_dict = {
+        "case_type":
+            "performance",
+        "dataset_path":
+            os.path.join(DATASET_PATH, "simulate_truth"),
+        "request_conf":
+            "vllm_api_stream_chat",
+        "dataset_conf":
+            "textvqa/textvqa_gen",
+        "num_prompts":
+            200,
+        "max_out_len":
+            256,
+        "batch_size":
+            128,
+        "temperature":
+            0.5,
+        "top_k":
+            10,
+        "top_p":
+            0.7,
+        "repetition_penalty":
+            1.2,
+        "request_rate":
+            0.28,
+        "baseline":
+            1,
+        "seed":
+            77,
+        "result_file_name":
+            "qwen2_5_vl_7b_perf_custom_1E1PD_merge",
+        "threshold":
+            0.97
+    }
+    aisbench_cases = []
+    for i in range(len(request_rate)):
+        case_dict["request_rate"] = request_rate[i]
+        new_case_dict = copy.deepcopy(case_dict)
+        aisbench_cases.append(new_case_dict)
+    api_port = 10001
+    async with RemoteEPDServer(start_mode="http",
+                               api_server_port=api_port,
+                               pd_num=1,
+                               e_num=1,
+                               env_dict=env_dict,
+                               e_serve_args=e_server_args,
+                               pd_serve_args=pd_server_args) as server:
+        # warm up
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           aisbench_cases=warmup_cases, verify=False, save=False)
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           aisbench_cases=aisbench_cases)
