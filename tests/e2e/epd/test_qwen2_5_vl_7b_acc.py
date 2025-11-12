44ecf0d3ce2e169525838482545e0130074922dc
wangyu31577 <wangyu31577@hundsun.com>
2025-11-12 09:45:50 +0800

    增加精度用例
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

---
+++ tests/e2e/epd/test_qwen2_5_vl_7b_acc.py
@@ -0,0 +1,175 @@
+import os
+
+import pytest
+
+from tests.e2e.conftest import RemoteEPDServer
+from tests.e2e.conftest import RemoteOpenAIServer
+from tests.e2e.epd.conftest import load_config
+from tools.aisbench import run_aisbench_cases
+
+model_path = load_config().get("model_path")
+MODELS = [os.path.join(model_path, "Qwen2.5-VL-7B-Instruct")]
+DATASET_PATH = load_config().get("dataset_path")
+
+TENSOR_PARALLELS = [1]
+DATASET_NAME = ["textvqa_subset"]
+
+SHARED_STORAGE_PATH = "/dev/shm/epd/storage"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+@pytest.mark.parametrize("dataset_name", DATASET_NAME)
+async def test_pd_mix_001(model: str, tp_size: int, dataset_name: str):
+    api_port = 10001
+    vllm_server_args = [
+        "--port",
+        str(api_port), "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "10000", "--max-num-batched-tokens",
+        "10000", "--max-num-seqs", "100", "--enforce-eager",
+        "--gpu-memory-utilization", "0.95"
+    ]
+    aisbench_cases = [{
+        "case_type": "accuracy",
+        "dataset_path": os.path.join(DATASET_PATH, dataset_name),
+        "request_conf": "vllm_api_stream_chat",
+        "dataset_conf": "textvqa/textvqa_gen",
+        "num_prompts": 200,
+        "max_out_len": 256,
+        "batch_size": 128,
+        "temperature": 0.5,
+        "top_k": 10,
+        "top_p": 0.7,
+        "repetition_penalty": 1.2,
+        "request_rate": 0,
+        "baseline": 1,
+        "seed": 77,
+        "threshold": 0.97
+    }]
+
+    with RemoteOpenAIServer(model,
+                            vllm_server_args,
+                            server_host="127.0.0.1",
+                            server_port=api_port,
+                            auto_port=False) as server:
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           save=False,
+                           aisbench_cases=aisbench_cases)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+@pytest.mark.parametrize("dataset_name", DATASET_NAME)
+async def test_1e1pd_001(model: str, tp_size: int, dataset_name: str):
+    e_server_args = [
+        "--no-enable-prefix-caching", "--model", model,
+        "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "10000", "--max-num-batched-tokens",
+        "10000", "--max-num-seqs", "1", "--enforce-eager",
+        "--gpu-memory-utilization", "0.0", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_producer"}'
+    ]
+    pd_server_args = [
+        "--model", model, "--max-model-len", "10000",
+        "--max-num-batched-tokens", "10000", "--tensor-parallel-size",
+        str(tp_size), "--max-num-seqs", "100", "--gpu-memory-utilization",
+        "0.95", "--enforce-eager", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_consumer"}'
+    ]
+    aisbench_cases = [{
+        "case_type": "accuracy",
+        "dataset_path": os.path.join(DATASET_PATH, dataset_name),
+        "request_conf": "vllm_api_stream_chat",
+        "dataset_conf": "textvqa/textvqa_gen",
+        "num_prompts": 200,
+        "max_out_len": 256,
+        "batch_size": 128,
+        "temperature": 0.5,
+        "top_k": 10,
+        "top_p": 0.7,
+        "repetition_penalty": 1.2,
+        "request_rate": 0,
+        "baseline": 1,
+        "seed": 77,
+        "threshold": 0.97
+    }]
+
+    api_port = 10001
+    async with RemoteEPDServer(start_mode="http",
+                               api_server_port=api_port,
+                               pd_num=1,
+                               e_num=1,
+                               e_serve_args=e_server_args,
+                               pd_serve_args=pd_server_args) as server:
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           save=False,
+                           aisbench_cases=aisbench_cases)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+@pytest.mark.parametrize("dataset_name", DATASET_NAME)
+async def test_1e2pd_001(model: str, tp_size: int,dataset_name: str, teardown):
+    e_server_args = [
+        "--no-enable-prefix-caching", "--model", model,
+        "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "10000", "--max-num-batched-tokens",
+        "10000", "--max-num-seqs", "1", "--enforce-eager",
+        "--gpu-memory-utilization", "0.0", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_producer"}'
+    ]
+
+    pd_server_args = [
+        "--model", model, "--max-model-len", "10000",
+        "--max-num-batched-tokens", "10000", "--tensor-parallel-size",
+        str(tp_size), "--max-num-seqs", "100", "--gpu-memory-utilization",
+        "0.95", "--enforce-eager", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"shared_storage_path":"' +
+        SHARED_STORAGE_PATH +
+        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_consumer"}'
+    ]
+
+    aisbench_cases = [{
+        "case_type": "accuracy",
+        "dataset_path": os.path.join(DATASET_PATH, dataset_name),
+        "request_conf": "vllm_api_stream_chat",
+        "dataset_conf": "textvqa/textvqa_gen",
+        "num_prompts": 200,
+        "max_out_len": 256,
+        "batch_size": 128,
+        "temperature": 0.5,
+        "top_k": 10,
+        "top_p": 0.7,
+        "repetition_penalty": 1.2,
+        "request_rate": 0,
+        "baseline": 1,
+        "seed": 77,
+        "threshold": 0.97
+    }]
+
+    api_port = 10001
+    async with RemoteEPDServer(start_mode="http",
+                               api_server_port=api_port,
+                               pd_num=2,
+                               e_num=1,
+                               e_serve_args=e_server_args,
+                               pd_serve_args=pd_server_args) as server:
+
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           save=False,
+                           aisbench_cases=aisbench_cases)
