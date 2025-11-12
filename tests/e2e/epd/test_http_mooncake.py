a5f91219f00d22c1383f15ad6f1017a1975248a1
wangyu31577 <wangyu31577@hundsun.com>
2025-11-12 18:36:34 +0800

    增加mooncake用例
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/epd/test_http_mooncake.py
+++ tests/e2e/epd/test_http_mooncake.py
@@ -15,36 +15,45 @@ MODELS = [os.path.join(model_path, "Qwen2.5-VL-7B-Instruct")]
 DATASET_PATH = load_config().get("dataset_path")
 
 TENSOR_PARALLELS = [1]
-#DATASET_NAME = ["simulate_truth"]
-DATASET_NAME = ["image_2", "image_3", "image_4"]
+DATASET_NAME = ["simulate_truth"]
 
-SHARED_STORAGE_PATH = "/dev/shm/epd/storage"
-
+MOONCAKE_PRODUCER_CONFIG_PATH = load_config().get("mooncake_config_path") + "producer.json"
+MOONCAKE_CONSUMER_CONFIG_PATH = load_config().get("mooncake_config_path") + "consumer.json"
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
-async def test_1e1pd_001_mooncake(model: str, tp_size: int, dataset_name: str):
+async def test_1e2pd_001_mooncake(model: str, tp_size: int, dataset_name: str):
+    env_dict = {}
+    env_dict["VLLM_NIXL_SIDE_CHANNEL_PORT"] = "6000"
     e_server_args = [
-        "--no-enable-prefix-caching", "--model", model,
-        "--tensor-parallel-size",
-        str(tp_size), "--max-model-len", "10000", "--max-num-batched-tokens",
-        "10000", "--max-num-seqs", "1", "--enforce-eager",
-        "--gpu-memory-utilization", "0.0", "--ec-transfer-config",
-        '{"ec_connector_extra_config":{"shared_storage_path":"' +
-        SHARED_STORAGE_PATH +
-        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_producer"}'
+        model, "--gpu-memory-utilization", "0.0", "--port", "19535"
+        "--tensor-parallel-size",str(tp_size), "--enforce-eager", "--enable-request-id-headers",
+        "--no-enable-prefix-caching",
+        "--max-model-len", "10000", "--max-num-batched-tokens",
+        "10000", "--max-num-seqs", "1",
+        "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"ec_mooncake_config_file_path":"' +
+        MOONCAKE_PRODUCER_CONFIG_PATH +
+        '", "ec_max_num_scheduled_tokens": "1000000000000000000"},"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_producer"}'
     ]
 
     pd_server_args = [
-        "--model", model, "--max-model-len", "10000",
-        "--max-num-batched-tokens", "10000", "--tensor-parallel-size",
-        str(tp_size), "--max-num-seqs", "100", "--gpu-memory-utilization",
-        "0.95", "--enforce-eager", "--ec-transfer-config",
-        '{"ec_connector_extra_config":{"shared_storage_path":"' +
-        SHARED_STORAGE_PATH +
-        '"},"ec_connector":"ECSharedStorageConnector","ec_role": "ec_consumer"}'
+        model, "--gpu-memory-utilization", "0.95", "--port", "19536"
+        "--tensor-parallel-size", str(tp_size), "--enforce-eager", "--enable-request-id-headers",
+        "--max-model-len", "10000", "--max-num-batched-tokens",
+        "10000", "--max-num-seqs", "128",
+        "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"ec_mooncake_config_file_path":"' +
+        MOONCAKE_CONSUMER_CONFIG_PATH +
+        '"},"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_consumer"}'
+    ]
+
+    mooncake_args = [
+        "--rpc_port", "50051", "--enable_http_metadata_server=true", "--http_metadata_server_host=0.0.0.0",
+        "--http_metadata_server_port=8081", "--rpc_thread_num", "8", "--default_kv_lease_ttl", "0",
+        "eviction_ratio", "0.05", "--eviction_high_watermark_ratio", "0.9"
     ]
 
     warmup_cases = [{
@@ -92,7 +101,7 @@ async def test_1e1pd_001_mooncake(model: str, tp_size: int, dataset_name: str):
         "request_rate": 0.28,
         "baseline": 1,
         "seed": 77,
-        "result_file_name": f"qwen2_5_vl_7b_{dataset_name}_1E1PD_sc",
+        "result_file_name": f"{dataset_name}_1E2PD_mooncake",
         "threshold": 0.97
     }
     aisbench_cases = []
@@ -104,11 +113,11 @@ async def test_1e1pd_001_mooncake(model: str, tp_size: int, dataset_name: str):
     api_port = 10001
     async with RemoteEPDServer(run_mode="zmq_proxy_server",
                                api_server_port=api_port,
-                               pd_num=1,
+                               pd_num=2,
                                e_num=1,
-                               is_epd_same_card=True,
                                e_serve_args=e_server_args,
-                               pd_serve_args=pd_server_args) as server:
+                               pd_serve_args=pd_server_args,
+                               mooncake_args=mooncake_args) as server:
 
         # warm up
         run_aisbench_cases(model=model,
