c2b8aef343b3a6f3fc1f6649991b03648d151ebd
wangyu31577 <wangyu31577@hundsun.com>
2025-11-20 23:43:46 +0800

    增加跨机
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -266,7 +266,7 @@ class RemoteEPDServer:
             for key, value in env_dict.items():
                 docker_cmd.extend(["-e", f"{key}={value}"])
 
-        docker_cmd.extend(["-e", "LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/openssl-3.2.6/lib:$LD_LIBRARY_PATH"])
+        docker_cmd.extend(["-e", "LD_LIBRARY_PATH='/usr/local/lib64:/usr/local/openssl-3.2.6/lib:$LD_LIBRARY_PATH'"])
         docker_cmd.append(container_name)
         docker_cmd.extend(server_cmd)
 
