ac1ddceda4b488c2c8c511d722e4973fb84a309c
wangyu31577 <wangyu31577@hundsun.com>
2025-11-24 19:29:38 +0800

    更新CI用例
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/epd/test_qwen2_5_vl_7b_perf.py
+++ tests/e2e/epd/test_qwen2_5_vl_7b_perf.py
@@ -39,7 +39,7 @@ REQUEST_CONFIG = [[0.3, 180], [0.6, 400], [1, 480], [1.5, 480], [2, 500]]
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
-@pytest.mark.parametrize("request_rate", REQUEST_CONFIG)
+@pytest.mark.parametrize("request_config", REQUEST_CONFIG)
 async def test_pd_mix_001(model: str, tp_size: int, dataset_name: str,
                           request_config: list,
                           teardown):
@@ -110,6 +110,7 @@ REQUEST_CONFIG = [[1.2, 900], [2.4, 1000], [4, 1100], [6, 1200], [8, 1300]]
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
+@pytest.mark.parametrize("request_config", REQUEST_CONFIG)
 async def test_1e3pd_001(model: str, tp_size: int, dataset_name: str,
                          request_config:list,
                          teardown):
@@ -200,6 +201,7 @@ REQUEST_CONFIG = [[0.9, 600], [1.8, 800], [3, 900], [4.5, 900], [6, 900]]
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
+@pytest.mark.parametrize("request_config", REQUEST_CONFIG)
 async def test_1e2pd_001(model: str, tp_size: int, dataset_name: str,
                          request_config: list,
                          teardown):
