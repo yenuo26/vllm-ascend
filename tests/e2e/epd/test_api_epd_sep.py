5e475d91dcc3bc7c960263feb8a90cc2f0aaca79
wangyu31577 <wangyu31577@hundsun.com>
2025-11-25 21:59:05 +0800

    适配mooncake全分离
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/epd/test_api_epd_sep.py
+++ tests/e2e/epd/test_api_epd_sep.py
@@ -6,21 +6,21 @@ import pytest
 from tests.e2e.conftest import RemoteEPDServer
 from tests.e2e.epd.conftest import load_config
 from tools.aisbench import run_aisbench_cases
+from tests.e2e.nightly.multi_node.config.utils import get_cluster_ips
 
 model_path = load_config().get("model_path")
 MODELS = [os.path.join(model_path, "Qwen2.5-VL-7B-Instruct")]
 DATASET_PATH = load_config().get("dataset_path")
 
 TENSOR_PARALLELS = [1]
-DATASET_NAME = ["simulate_truth"]
 
-MOONCAKE_PRODUCER_CONFIG_PATH = load_config().get(
-    "mooncake_config_path") + "producer.json"
-MOONCAKE_CONSUMER_CONFIG_PATH = load_config().get(
-    "mooncake_config_path") + "consumer.json"
 SHARED_STORAGE_PATH = "/dev/shm/epd/storage"
+ENABLE_PREFIX = [True, False]
 
 
+
+
+DATASET_NAME = ["simulate_truth"]
 @pytest.mark.asyncio
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
@@ -91,7 +91,7 @@ async def test_1e1p1d_ipc_storage_mooncake_001(model: str, tp_size: int,
         "seed": 77,
     }]
 
-    request_rate = [0.42, 1.17, 1.92, 2.67]
+    request_rate = [5.34]
     case_dict = {
         "case_type": "performance",
         "dataset_path": os.path.join(DATASET_PATH, dataset_name),
@@ -141,13 +141,18 @@ async def test_1e1p1d_ipc_storage_mooncake_001(model: str, tp_size: int,
                            aisbench_cases=aisbench_cases)
 
 
+REQUEST_RATE = [0.28, 0.78, 1.28, 1.78]
+DATASET_NAME = ["image_4", "simulate_truth"]
 @pytest.mark.asyncio
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
+@pytest.mark.parametrize("request_rate", REQUEST_RATE)
+@pytest.mark.parametrize("enable_prefix", ENABLE_PREFIX)
 async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
-                                       dataset_name: str):
+                                       dataset_name: str, request_rate:float, enable_prefix:bool):
     env_dict = {}
+    cluster_ip = get_cluster_ips()
     env_dict["VLLM_NIXL_SIDE_CHANNEL_PORT"] = "6000"
     e_server_args = [
         "--model", model, "--gpu-memory-utilization", "0.0",
@@ -155,9 +160,11 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
         str(tp_size), "--enforce-eager", "--no-enable-prefix-caching",
         "--max-model-len", "10000", "--max-num-batched-tokens", "10000",
         "--max-num-seqs", "1", "--ec-transfer-config",
-        '{"ec_connector_extra_config":{"ec_mooncake_config_file_path":"' +
-        MOONCAKE_PRODUCER_CONFIG_PATH +
-        '", "ec_max_num_scheduled_tokens": "1000000000000000000"},"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_producer"}'
+        f'{{"ec_connector_extra_config":{{"local_hostname":"{cluster_ip[0]}","metadata_server": "http://{cluster_ip[0]}:8082/metadata",'
+        f'"global_segment_size": 32212254720, "local_buffer_size": 1073741824, "protocol": "tcp", "device_name": "",'
+        f'"master_server_address": "{cluster_ip[0]}:50052","replica_num": 1, "fast_transfer":true, "fast_transfer_buffer_size": 1'
+        '"ec_max_num_scheduled_tokens": "1000000000000000000"},'
+        '"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_producer"}'
     ]
 
     pd_server_args = [
@@ -167,9 +174,11 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
             str(tp_size), "--enforce-eager", "--max-model-len", "10000",
             "--max-num-batched-tokens", "10000", "--max-num-seqs", "128",
             "--ec-transfer-config",
-            '{"ec_connector_extra_config":{"ec_mooncake_config_file_path":"' +
-            MOONCAKE_CONSUMER_CONFIG_PATH +
-            '"},"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_consumer"}',
+            f'{{"ec_connector_extra_config":{{"local_hostname":"{cluster_ip[0]}","metadata_server": "http://{cluster_ip[0]}:8082/metadata",'
+            f'"global_segment_size": 32212254720, "local_buffer_size": 1073741824, "protocol": "tcp", "device_name": "",'
+            f'"master_server_address": "{cluster_ip[0]}:50052","replica_num": 1, "fast_transfer":true, "fast_transfer_buffer_size": 1'
+            '"ec_max_num_scheduled_tokens": "1000000000000000000"},'
+            '"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_consumer"}'
             "--kv-transfer-config",
             '{"kv_connector": "MooncakeConnectorStoreV1","kv_role": "kv_producer","mooncake_rpc_port": "50052"}'
             '"kv_connector_extra_config": {"local_hostname": "localhost", "metadata_server": "http://localhost:8082/metadata",'
@@ -187,13 +196,19 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
         ]
     ]
 
-    mooncake_args = [
+    mooncake_args = [[
         "--rpc_port", "50051", "--enable_http_metadata_server=true",
         "--http_metadata_server_host=0.0.0.0",
         "--http_metadata_server_port=8081", "--rpc_thread_num", "8",
         "--default_kv_lease_ttl", "10000", "eviction_ratio", "0.05",
         "--eviction_high_watermark_ratio", "0.9"
-    ]
+    ],[
+        "--rpc_port", "50052", "--enable_http_metadata_server=true",
+        "--http_metadata_server_host=0.0.0.0",
+        "--http_metadata_server_port=8082", "--rpc_thread_num", "8",
+        "--default_kv_lease_ttl", "10000", "eviction_ratio", "0.05",
+        "--eviction_high_watermark_ratio", "0.9"
+    ]]
 
     warmup_cases = [{
         "case_type": "performance",
@@ -211,8 +226,7 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
         "seed": 77,
     }]
 
-    request_rate = [0.42, 1.17, 1.92, 2.67]
-    case_dict = {
+    aisbench_cases = [{
         "case_type": "performance",
         "dataset_path": os.path.join(DATASET_PATH, dataset_name),
         "request_conf": "vllm_api_stream_chat",
@@ -223,18 +237,12 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
         "top_k": 10,
         "top_p": 0.7,
         "repetition_penalty": 1.2,
-        "request_rate": 0.28,
+        "request_rate": request_rate*3,
         "baseline": 1,
         "seed": 77,
         "result_file_name": f"{dataset_name}_1E1P1D_mooncake",
         "threshold": 0.97
-    }
-    aisbench_cases = []
-    for i in range(len(request_rate)):
-        case_dict["request_rate"] = request_rate[i]
-        new_case_dict = copy.deepcopy(case_dict)
-        aisbench_cases.append(new_case_dict)
-
+    }]
     api_port = 10001
     async with RemoteEPDServer(run_mode="worker",
                                store_type="mooncake",
