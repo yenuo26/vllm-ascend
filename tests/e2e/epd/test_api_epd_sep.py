5cab64a5908c0c2f334486854461ba5f71dc97b5
wangyu31577 <wangyu31577@hundsun.com>
2025-11-26 15:15:37 +0800

    修改epd用例
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/epd/test_api_epd_sep.py
+++ tests/e2e/epd/test_api_epd_sep.py
@@ -22,6 +22,7 @@ ROUTER = ["RandomRouter", "RoundRobinRouter", "LeastInFlightRouter"]
 
 
 @pytest.mark.asyncio
+@pytest.mark.perf
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
@@ -152,6 +153,7 @@ async def test_1e1p1d_ipc_storage_mooncake_001(model: str, tp_size: int,
 REQUEST_RATE = [0.28, 0.78, 1.28, 1.78]
 DATASET_NAME = ["image_4", "simulate_truth"]
 @pytest.mark.asyncio
+@pytest.mark.perf
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
 @pytest.mark.parametrize("dataset_name", DATASET_NAME)
@@ -161,13 +163,13 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
                                        dataset_name: str, request_rate: float, router: str):
     '''
     数据集： simulate_truth、image_4
-    前缀缓存：开启/关闭
     部署形态： 1E1P1D、单机
     存储类型：EC mooncake , KV mooncake
     调度策略：RandomRouter， RoundRobinRouter，LeastInFlightRouter
     '''
     env_dict = {}
     env_dict["VLLM_NIXL_SIDE_CHANNEL_PORT"] = "6000"
+    env_dict["LM_SERVICE_REQUEST_TIMEOUT_SECONDS"] = "300"
     e_server_args = [
         "--model", model, "--gpu-memory-utilization", "0.0",
         "--tensor-parallel-size",
@@ -292,4 +294,148 @@ async def test_1e1p1d_ipc_mooncake_001(model: str, tp_size: int,
         run_aisbench_cases(model=model,
                            port=api_port,
                            card_num=3,
+                           aisbench_cases=aisbench_cases)
+
+
+
+
+REQUEST_RATE = [0.28, 0.78, 1.28, 1.78]
+DATASET_NAME = ["simulate_truth"]
+@pytest.mark.asyncio
+@pytest.mark.perf
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("tp_size", TENSOR_PARALLELS)
+@pytest.mark.parametrize("dataset_name", DATASET_NAME)
+@pytest.mark.parametrize("request_rate", REQUEST_RATE)
+async def test_2e3p3d_tcp_mooncake_001(model: str, tp_size: int,
+                                       dataset_name: str, request_rate: float):
+    '''
+    数据集： simulate_truth、image_4
+    部署形态： 2E3P3D、单机
+    存储类型：EC mooncake , KV mooncake
+    '''
+    env_dict = {}
+    env_dict["VLLM_NIXL_SIDE_CHANNEL_PORT"] = "6000"
+    env_dict["LM_SERVICE_REQUEST_TIMEOUT_SECONDS"] = "300"
+    e_server_args = [
+        "--model", model, "--gpu-memory-utilization", "0.0",
+        "--tensor-parallel-size",
+        str(tp_size), "--enforce-eager", "--no-enable-prefix-caching",
+        "--max-model-len", "10000", "--max-num-batched-tokens", "10000",
+        "--max-num-seqs", "1", "--ec-transfer-config",
+        '{"ec_connector_extra_config":{"local_hostname":"localhost",'
+        '"metadata_server": "http://localhost:8085/metadata","global_segment_size": 32212254720, '
+        '"local_buffer_size": 1073741824, "protocol": "tcp", "device_name": "",'
+        '"master_server_address": "localhost:50055","replica_num": 1, "fast_transfer":true, '
+        '"fast_transfer_buffer_size": 1, "ec_max_num_scheduled_tokens": "1000000000000000000"},'
+        '"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_producer"}'
+    ]
+
+    pd_server_args = [
+        [
+            "--model", model, "--gpu-memory-utilization", "0.95",
+            "--tensor-parallel-size",
+            str(tp_size), "--enforce-eager", "--max-model-len", "10000",
+            "--max-num-batched-tokens", "10000", "--max-num-seqs", "128",
+            "--ec-transfer-config",
+            '{"ec_connector_extra_config":{"local_hostname":"localhost",'
+            '"metadata_server": "http://localhost:8085/metadata","global_segment_size": 0, '
+            '"local_buffer_size": 1073741824, "protocol": "tcp", "device_name": "",'
+            '"master_server_address": "localhost:50055","replica_num": 1, "fast_transfer":true, '
+            '"fast_transfer_buffer_size": 1},'
+            '"ec_connector":"ECMooncakeStorageConnector","ec_role": "ec_consumer"}',
+            "--kv-transfer-config",
+            '{"kv_connector_extra_config": {"local_hostname": "localhost", '
+            '"metadata_server": "http://localhost:8081/metadata","protocol": "tcp", '
+            '"device_name": "", "master_server_address": "localhost:50051", '
+            '"global_segment_size": 30000000000},"kv_connector": "MooncakeConnectorStoreV1", '
+            '"kv_role": "kv_producer", "mooncake_rpc_port": "50051"}'
+        ],
+        [
+            "--model", model, "--gpu-memory-utilization", "0.95",
+            "--tensor-parallel-size",
+            str(tp_size), "--enforce-eager", "--max-model-len", "10000",
+            "--max-num-batched-tokens", "10000", "--max-num-seqs", "128",
+            "--kv-transfer-config",
+            '{"kv_connector_extra_config": {"local_hostname": "localhost", '
+            '"metadata_server": "http://localhost:8081/metadata","protocol": "tcp", '
+            '"device_name": "", "master_server_address": "localhost:50051", '
+            '"global_segment_size": 30000000000},"kv_connector": "MooncakeConnectorStoreV1", '
+            '"kv_role": "kv_consumer", "mooncake_rpc_port": "50051"}'
+        ]
+    ]
+
+    mooncake_args = [
+        [
+            "--rpc_port", "50051", "--enable_http_metadata_server=true",
+            "--http_metadata_server_host=0.0.0.0",
+            "--http_metadata_server_port=8081", "--rpc_thread_num", "8",
+            "--default_kv_lease_ttl", "10000", "eviction_ratio", "0.05",
+            "--eviction_high_watermark_ratio", "0.9", "--metrics_port", "9005"
+        ],
+        [
+            "--rpc_port", "50055", "--enable_http_metadata_server=true",
+            "--http_metadata_server_host=0.0.0.0",
+            "--http_metadata_server_port=8085", "--rpc_thread_num", "8",
+            "--default_kv_lease_ttl", "10000", "eviction_ratio", "0.05",
+            "--eviction_high_watermark_ratio", "0.9", "--metrics_port", "9004"
+        ]
+    ]
+
+    warmup_cases = [{
+        "case_type": "performance",
+        "dataset_path": os.path.join(DATASET_PATH, dataset_name),
+        "request_conf": "vllm_api_stream_chat",
+        "dataset_conf": "textvqa/textvqa_gen",
+        "num_prompts": 50,
+        "max_out_len": 256,
+        "batch_size": 16,
+        "temperature": 0.5,
+        "top_k": 10,
+        "top_p": 0.7,
+        "repetition_penalty": 1.2,
+        "request_rate": 0,
+        "seed": 77,
+    }]
+
+    aisbench_cases = [{
+        "case_type": "performance",
+        "dataset_path": os.path.join(DATASET_PATH, dataset_name),
+        "request_conf": "vllm_api_stream_chat",
+        "dataset_conf": "textvqa/textvqa_gen",
+        "num_prompts": 200,
+        "batch_size": 128,
+        "temperature": 0.5,
+        "top_k": 10,
+        "top_p": 0.7,
+        "repetition_penalty": 1.2,
+        "request_rate": request_rate * 8,
+        "baseline": 1,
+        "seed": 77,
+        "result_file_name": f"{dataset_name}_2E3P3D_mooncake",
+        "threshold": 0.97
+    }]
+    api_port = 10001
+    async with RemoteEPDServer(run_mode="worker",
+                               store_type="mooncake",
+                               kv_store_type="mooncake",
+                               proxy_type="api_server",
+                               api_server_port=api_port,
+                               pd_num=6,
+                               e_num=2,
+                               env_dict=env_dict,
+                               e_serve_args=e_server_args,
+                               pd_serve_args=pd_server_args,
+                               mooncake_args=mooncake_args) as server:
+
+        # warm up
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           aisbench_cases=warmup_cases,
+                           verify=False,
+                           save=False)
+        # aisbench test
+        run_aisbench_cases(model=model,
+                           port=api_port,
+                           card_num=8,
                            aisbench_cases=aisbench_cases)
