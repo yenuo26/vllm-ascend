0f85eabbfe192a58389f57db7e7d178ae690f036
wangyu31577 <wangyu31577@hundsun.com>
2025-11-28 14:59:56 +0800

    优化api_server调用逻辑
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -538,10 +538,42 @@ class ApiServer:
                 loop="asyncio",
                 log_config={
                     "version": 1,
+                    "disable_existing_loggers": False,
                     "formatters": {
-                        "default": {"fmt": "[PROXY ] %(message)s"},
-                        "access": {"fmt": '[PROXY ] %(client_addr)s - "%(request_line)s" %(status_code)s'},
-                    }
+                        "default": {
+                            "fmt": "[PROXY ] %(message)s"
+                        },
+                        "access": {
+                            "fmt": '[PROXY ] %(client_addr)s - "%(request_line)s" %(status_code)s'
+                        },
+                    },
+                    "handlers": {
+                        "default": {
+                            "formatter": "default",
+                            "class": "logging.StreamHandler",
+                            "stream": "ext://sys.stderr"
+                        },
+                        "access": {
+                            "formatter": "access",
+                            "class": "logging.StreamHandler",
+                            "stream": "ext://sys.stdout"
+                        },
+                    },
+                    "loggers": {
+                        "uvicorn": {
+                            "handlers": ["default"],
+                            "level": "INFO",
+                            "propagate": False
+                        },
+                        "uvicorn.error": {
+                            "level": "INFO"
+                        },
+                        "uvicorn.access": {
+                            "handlers": ["access"],
+                            "level": "INFO",
+                            "propagate": False
+                        },
+                    },
                 }
             )
             server = uvicorn.Server(config)
