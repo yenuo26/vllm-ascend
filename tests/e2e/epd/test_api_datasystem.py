b259e073b6075fef0576e3673d839eefb345a819
z00939163 <zhumingjue@huawei.com>
2025-12-16 11:04:44 +0800

    新增ds性能测试用例

--- tests/e2e/epd/test_api_datasystem.py
+++ tests/e2e/epd/test_api_datasystem.py
@@ -421,6 +421,8 @@ async def test_proxy_1e_2pd_cross_datasystem_tcp_ipv4_001(model: str, tp_size: i
         cluster.add_node_info("pd", 2, CONTAINER_NAME)
     # for i in range(d_num):
     #     cluster.add_node_info("d", 1, CONTAINER_NAME)
+    cluster.add_node_info("ds", 1, CONTAINER_NAME)
+    cluster.add_node_info("ds", 2, CONTAINER_NAME)
 
     for i in range(e_num):
         env_dict.add_env("e", "ASCEND_RT_VISIBLE_DEVICES", str(i))
